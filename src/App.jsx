c7a2924edcbac227333ddb598f7a339519ac0b4d
“Matt” <mhallrp@gmail.com>
2023-12-26 20:13:40 +0100

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Landing from './Pages/Landing/index';
+import Landing from './Pages/Landing';
+import NewPage from './Pages/NewPage';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/new" element={ <NewPage /> }/>
+          <Route path="/" element={ <Landing /> }/>
+      </Routes>
+    </ BrowserRouter>
   );
 }
 
---
+++ src/Pages/NewPage/NewPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const NewPage = () =>{
+    return ( 
+        <h1>New Page</h1>
+    )
+}
+
+export default NewPage
