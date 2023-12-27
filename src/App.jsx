9fbded35cfc66df88f9f200aacd38aa507f927e2
“Matt” <mhallrp@gmail.com>
2023-12-27 14:24:31 +0100

    removed login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import Landing from './Pages/Landing';
-import Login from './Pages/Login';
-
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/login" element={ <Login /> }/>
-          <Route path="/" element={ <Landing /> }/>
-      </Routes>
-    </ BrowserRouter>
+    <Landing />
   );
 }
 
